3021566caf4b7469d0f3691b36210f89a95528f6
Nicholas Herrick <nth.herrick@gmail.com>
2020-04-24 16:33:06 -0600

    updated resume and email

--- public/js/app.js
+++ public/js/app.js
@@ -1,118 +1,137 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
   AOS.init();
-  
-  $('.nav-item [href^="/home' + location.pathname.split("/")[1] + '"]').addClass(
-    "active"
-  );
-  $('.nav-item [href^="/home' + location.pathname.split("/home")[1] + '"]').addClass(
-    "active"
-  );
-  $('.nav-item [href^="/portfolio' + location.pathname.split("/portfolio")[1] + '"]').addClass(
-    "active"
-  );
-  $('.nav-item [href^="/about' + location.pathname.split("/about")[1] + '"]').addClass(
-    "active"
-  );
-  $('.nav-item [href^="/contact' + location.pathname.split("/contact")[1] + '"]').addClass(
-    "active"
-  );
-  $('.nav-item [href^="/web-development' + location.pathname.split("/web-development")[1] + '"]').addClass(
-    "active"
-  );
-  $('.dropdown-item [href^="/web-development-page' + location.pathname.split("/web-development-page")[1] + '"]').addClass(
-    "active"
-  );
-  $('.dropdown-item [href^="/web-development/front-end' + location.pathname.split("/web-development/front-end")[1] + '"]').addClass(
-    "active"
-  );
-  $('.dropdown-item [href^="/web-development/back-end' + location.pathname.split("/web-development/back-end")[1] + '"]').addClass(
-    "active"
-  );
-  $('.dropdown-item [href^="/web-development/ui-ux' + location.pathname.split("/web-development/ui-ux")[1] + '"]').addClass(
-    "active"
-  );
-  $('.nav-item [href^="/other-skills' + location.pathname.split("/other-skills")[1] + '"]').addClass(
-    "active"
-  );
+
+  $(
+    '.nav-item [href^="/home' + location.pathname.split('/')[1] + '"]'
+  ).addClass('active');
+  $(
+    '.nav-item [href^="/home' + location.pathname.split('/home')[1] + '"]'
+  ).addClass('active');
+  $(
+    '.nav-item [href^="/portfolio' +
+      location.pathname.split('/portfolio')[1] +
+      '"]'
+  ).addClass('active');
+  $(
+    '.nav-item [href^="/about' + location.pathname.split('/about')[1] + '"]'
+  ).addClass('active');
+  $(
+    '.nav-item [href^="/contact' + location.pathname.split('/contact')[1] + '"]'
+  ).addClass('active');
+  $(
+    '.nav-item [href^="/web-development' +
+      location.pathname.split('/web-development')[1] +
+      '"]'
+  ).addClass('active');
+  $(
+    '.dropdown-item [href^="/web-development-page' +
+      location.pathname.split('/web-development-page')[1] +
+      '"]'
+  ).addClass('active');
+  $(
+    '.dropdown-item [href^="/web-development/front-end' +
+      location.pathname.split('/web-development/front-end')[1] +
+      '"]'
+  ).addClass('active');
+  $(
+    '.dropdown-item [href^="/web-development/back-end' +
+      location.pathname.split('/web-development/back-end')[1] +
+      '"]'
+  ).addClass('active');
+  $(
+    '.dropdown-item [href^="/web-development/ui-ux' +
+      location.pathname.split('/web-development/ui-ux')[1] +
+      '"]'
+  ).addClass('active');
+  $(
+    '.nav-item [href^="/other-skills' +
+      location.pathname.split('/other-skills')[1] +
+      '"]'
+  ).addClass('active');
 
   // Login Button
-  $(document.body).on("click", "#login-button", function (event) {
+  $(document.body).on('click', '#login-button', function(event) {
     event.preventDefault();
     var adminData = {
-      username: $("#username")
+      username: $('#username')
         .val()
         .trim(),
-      password: $("#password")
+      password: $('#password')
         .val()
-        .trim()
+        .trim(),
     };
-    $.post("/api/login", adminData)
-      .then(function (res) {
+    $.post('/api/login', adminData)
+      .then(function(res) {
         window.location.replace(res);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log(err.responseJSON);
       });
   });
 
-  $(document.body).on("click", "#submit-project", function (event) {
+  $(document.body).on('click', '#submit-project', function(event) {
     event.preventDefault();
     var newProjectData = {
-      projectName: $("#project-name")
+      projectName: $('#project-name')
         .val()
         .trim(),
-      deployedLink: $("#deployed-link")
+      deployedLink: $('#deployed-link')
         .val()
         .trim(),
-      repositoryLink: $("#repository-link")
+      repositoryLink: $('#repository-link')
         .val()
         .trim(),
-      projectDescription: $("#project-description")
+      projectDescription: $('#project-description')
         .val()
         .trim(),
-      imagePath: $("#image-path")
+      imagePath: $('#image-path')
         .val()
-        .trim()
+        .trim(),
     };
-    $.post("/api/project", newProjectData)
-      .then(function (res) {
+    $.post('/api/project', newProjectData)
+      .then(function(res) {
         window.location.replace(res);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log(err.responseJSON);
       });
   });
 
-  $(document.body).on("click", "#send-email", function (event) {
+  $(document.body).on('click', '#send-email', function(event) {
     event.preventDefault();
     var msg = {
-      to: "nth.herrick@gmail.com",
-      from: "info@nicholasherrick.com",
-      subject: $("#email-subject").val().trim(),
-      text: $("#email-text").val().trim(),
-      html: "<strong>Sent from nicholasherrick.com</strong>"
+      to: 'nick@nicholasherrick.com',
+      from: 'info@nicholasherrick.com',
+      subject: $('#email-subject')
+        .val()
+        .trim(),
+      text: $('#email-text')
+        .val()
+        .trim(),
+      html: '<strong>Sent from nicholasherrick.com</strong>',
     };
-    $.post("/api/sendemail", msg).then(function (res) {
-      window.location.replace(res);
-    }).catch(function (err) {
-      console.log(err.responseJSON);
-    });
+    $.post('/api/sendemail', msg)
+      .then(function(res) {
+        window.location.replace(res);
+      })
+      .catch(function(err) {
+        console.log(err.responseJSON);
+      });
   });
 
   // Sends registration info from the login page
-  $(document.body).on("click", "#register-button", function(event) {
+  $(document.body).on('click', '#register-button', function(event) {
     event.preventDefault();
     var adminData = {
-      username: $("#username")
+      username: $('#username')
         .val()
         .trim(),
-      password: $("#password")
+      password: $('#password')
         .val()
-        .trim()
+        .trim(),
     };
     console.log(adminData);
-    $.post("/api/signup", adminData)
+    $.post('/api/signup', adminData)
       .then(function(res) {
         window.location.replace(res);
       })
@@ -121,5 +140,3 @@ $(document).ready(function () {
       });
   });
 });
-
-
